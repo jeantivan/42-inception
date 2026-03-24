99df5416ba99329ccfcf0a64513a3b7a34df98ce
Jean Tivan Rendon <jptivan53@gmail.com>
2026-03-24 17:07:51 +0100

    CHANGE: Better config for FTP service

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -2,41 +2,37 @@
 
 set -e
 
-echo "[Entrypoint] Starting vsftpd..."
+echo "[Entrypoint] Starting vsftpd service setup..."
+
+if [ ! -f /run/secrets/ftp_password ]; then
+	echo "[Entrypoint] ERROR: ftp_password is missing"
+	exit 1;
+fi
+
 
 FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
-if [ -z "$FTP_USER" ] || [ -z "$FTP_PASSWORD" ]; then
-	echo "Error: FTP_USER and FTP_PASSWORD environment variables must be set."
+if [ -z "$FTP_USER" ]; then
+	echo "[Entrypoint] ERROR: FTP_USER environment variables must be set."
 	exit 1
 fi
 
 if ! id "$FTP_USER" > /dev/null 2>&1; then
-	echo "[Entrypoint] Creating user $FTP_USER..."
+	echo "[Entrypoint] Creating user '$FTP_USER'..."
 
-	# Create USER
-	# -D: Don't create a home directory
-	# -h: Home directory
-	# -s /bin/false: Set the shell to /bin/false to prevent shell access
-	adduser -D -h /var/www -s /bin/false "$FTP_USER"
+	# -D: No defaults / -h: Directory home is the web / -s: No shell access
+	adduser -D -h /var/www/html -s /bin/false "$FTP_USER"
 
 	# Set user password
 	echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-	if getent group www-data > /dev/null 2>&1; then
-		echo "[Entrypoint] Adding $FTP_USER to www-data group..."
-		adduser "$FTP_USER" www-data
-	else
-		addgroup -g 82 www-data
-		adduser "$FTP_USER" www-data
-	fi
+	addgroup "FTP_USER" nobody 2>/dev/null || true
 fi
 
-
-mkdir -p /var/www
-chown -R "$FTP_USER":www-data /var/www/html
+mkdir -p /var/www/html
+chown -R "$FTP_USER":nobody /var/www/html
 chmod -R 775 /var/www/html
 
-echo "[Entrypoint] Config finished, starting vsftpd..."
+echo "[Entrypoint] FTP cconfig finished, starting vsftpd..."
 
 exec "$@"
